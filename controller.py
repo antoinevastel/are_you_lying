d9ec6d475cf1c1d039f54f105ad4ed45dbc69902
antoinevastel <antoine.vastel@gmail.com>
2017-04-03 18:29:52 +0200

    Add function to get HTTP headers

--- controller.py
+++ controller.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask import jsonify
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return render_template('display_fingerprint.html')
+
+
+@app.route('/headers')
+def get_headers():
+    print(request.headers)
+    headers = dict()
+    for header in request.headers:
+        headers[header[0]] = header[1]
+    return jsonify(headers)
