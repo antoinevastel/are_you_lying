b35a710651d527cb8e3800f8422b5b2aaff1a880
antoinevastel <antoine.vastel@gmail.com>
2017-05-06 19:32:07 +0200

    Adds detection of isolated cells in canvas

--- static/js/fingerprint.js
+++ static/js/fingerprint.js
@@ -21,7 +21,7 @@ function generateFingerprint(){
       getWebGL();
       fp.modernizr = testModernizr();
       fp.overwrittenObjects = testOverwrittenObjects();
-      testCanvasValue(canvasObj.data);
+      fp.canvasPixels = testCanvasValue(canvasObj.data);
 
       // New attributes
       fp.appCodeName = getAppCodeName();
@@ -770,50 +770,60 @@ function testCanvasValue(imgData){
   var height = 60;
   var width = 400;
   var binarizedImg = new Array(height + 2);
-  /* for(var i = 0; i < binarizedImg.length; i++){ */
-    // binarizedImg[i] = new Array(width + 2);
-    // break;
-  /* } */
+  for(var i = 0; i < binarizedImg.length; i++){
+    binarizedImg[i] = new Array(width + 2);
+  }
 
-  /* for(var j = 0; j < binarizedImg[0].length; j++){ */
-    // binarizedImg[0][j] = false;
-    // binarizedImg[binarizedImg.length - 1][j] = false;
-  /* } */
+  for(var j = 0; j < binarizedImg[0].length; j++){
+    binarizedImg[0][j] = false;
+    binarizedImg[binarizedImg.length - 1][j] = false;
+  }
 
-  /* for(var i = 0; i < binarizedImg.length; i++){ */
-    // binarizedImg[i][0] = false;
-    // binarizedImg[i][binarizedImg[0].length - 1] = false;
-  /* } */
+  for(var i = 0; i < binarizedImg.length; i++){
+    binarizedImg[i][0] = false;
+    binarizedImg[i][binarizedImg[0].length - 1] = false;
+  }
   console.log("test1");
   var binValue;
   nbZeroElts = 0;
-  var xi, yi;
-  /* for(var i = 0; i < imgData.length; i = i+4){ */
-    // r = imgData[i];
-    // g = imgData[i+1];
-    // b = imgData[i+2];
-    // a = imgData[i+3];
-    // if(r ==0 && g ==0 && b ==0 && a == 0){
-    //     binValue = false;
-    //     nbZeroElts++;
-    // } else{
-    //     binValue = true;
-    // }
-    // yi = i % (width) + 1;
-    // xi = i/width +1;
-    // binarizedImg[xi][yi] = binValue;
-  /* } */
+  var xi = 1, yi = 1;
+  for(var i = 0; i < imgData.length; i = i+4){
+    r = imgData[i];
+    g = imgData[i+1];
+    b = imgData[i+2];
+    a = imgData[i+3];
+    if(r ==0 && g ==0 && b ==0 && a == 0){
+        binValue = false;
+        nbZeroElts++;
+    } else{
+        binValue = true;
+    }
+    if((yi)% width == 0){
+        yi = 1;
+        xi++;
+    }else{
+        yi++;
+    }
+    //probably a problem with offsets, check it later...
+    binarizedImg[xi][yi] = binValue;
+  }
 
-  /* for(var i = 0; i < binarizedImg.length; i++){ */
-    // for(var j = 0; j < binarizedImg[0].length; j++){
-    //   var isolated = true;
-    //   if(binarizedImg[i][j] == true){
-    //     if(i > 0)
-    //   }
-    /* } */
-  // }
+  var isolatedCells = [];
+  for(var i = 1; i < binarizedImg.length-1; i++){
+    for(var j = 1; j < binarizedImg[0].length-1; j++){
+      var isolated = true;
+      if(binarizedImg[i][j] == true){
+        if(!binarizedImg[i-1][j-1] && !binarizedImg[i-1][j] && !binarizedImg[i-1][j+1] &&
+        !binarizedImg[i][j-1] && !binarizedImg[i][j+1] && !binarizedImg[i+1][j-1] && !binarizedImg[i+1][j] &&
+        !binarizedImg[i+1][j+1]){
+            //all cells are empty next to i,j
+            isolatedCells.push({x:i-1, y:j-1});
+        }
+      }
+    }
+  }
 
-  console.log("There are "+nbZeroElts+" zero elements");
+  return {nbZeros: nbZeroElts, isolatedCells: isolatedCells};
 }
 
 function testOverwrittenObjects(){
