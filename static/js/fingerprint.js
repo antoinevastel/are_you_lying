4257af91eb65af30e346557ffd57ffc864b029f7
antoinevastel <antoine.vastel@gmail.com>
2017-06-01 11:28:12 +0200

    update separators

--- static/js/fingerprint.js
+++ static/js/fingerprint.js
@@ -258,7 +258,7 @@ function getMimeTypes(){
 	var mimeTypes = [];
 	for(var i = 0; i < navigator.mimeTypes.length; i++){
 		var mt = navigator.mimeTypes[i];
-		mimeTypes.push([mt.description, mt.type, mt.suffixes].join("~"));
+		mimeTypes.push([mt.description, mt.type, mt.suffixes].join("~~"));
 	}
 	return mimeTypes.join(";;");
 }
